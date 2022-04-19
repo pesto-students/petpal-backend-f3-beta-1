85ad4c3a61b6ae88940baadac3a646a61e9bef18
atulkumbhar85 <atul.kumbhar1985@gmail.com>
2022-04-19 09:56:09 +0530

    minor change

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const authenticate = require('../middleware/Authenticate')
 const router = express.Router();
